09c25ab261ad9e719e4afd889f10dc959eeae0f5
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-02-27 15:16:13 +0000

    [Update]4-bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :book, only: [:new, :index, :show]
+  
+  resources :book, only: [:new, :create, :index, :show]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   
   get 'homes/about' => 'homes#about', as: 'about'
+  get 'books/new'
+  post 'book' =>'books#create'
   
-
 end
